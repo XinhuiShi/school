65d02ce3d3d3246654c4f76145dfbd40509d642a
XinhuiShi <54811159+XinhuiShi@users.noreply.github.com>
2021-02-24 19:53:16 -0500

    Add files via upload

---
+++ cen4802/src/week01/HelloWorld.java
@@ -0,0 +1,19 @@
+package week01;
+
+/**
+ * 
+ * @author Xinhui Shi
+ *
+ */
+public class HelloWorld {
+/**
+ * Just a simple display(). Only display it's content with no inputs.
+ * @return prompt + name
+ */
+	public String display() {
+		String name = "Xinhui Shi";
+		String prompt = "Hello World from";
+		return name + prompt;
+	}
+
+}
